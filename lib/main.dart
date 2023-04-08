0d8b196a95d1710c3bee15399c89b883aa0d67e5
abanoub emad <abanoubzaki14@gmail.com>
2023-04-08 15:03:34 +0200

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -23,26 +24,71 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 59, 166, 94),
-        title: const Text("Birthday Card"),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/imgs/1.jpeg",
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Happy Birthday !!',
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
+      // appBar: AppBar(
+      //   backgroundColor: Color.fromARGB(255, 59, 166, 94),
+      //   title: const Text("Birthday Card"),
+      // ),
+      body: Container(
+        color: Colors.teal,
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/imgs/2.JPG"),
+                radius: 100,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Abanoub Emad',
+                style: TextStyle(fontSize: 50, fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(fontSize: 30, color: Colors.white70),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 140.0,
+                  vertical: 12,
+                ),
+                child: Divider(
+                  thickness: 2,
+                  height: 1,
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                  vertical: 7,
+                ),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: ListTile(
+                    leading: Icon(Icons.phone),
+                    subtitle: Text("Phone Number"),
+                    title: Text("+20 1152560323"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                  vertical: 7,
+                ),
+                child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: ListTile(
+                      leading: Icon(Icons.mail),
+                      title: Text("AbanoubEmad@gmail.com"),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
       // floatingActionButton: FloatingActionButton(
