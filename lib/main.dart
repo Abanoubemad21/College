f53955b3b52eee67ac320410e86da089b4e1329e
abanoub emad <abanoubzaki14@gmail.com>
2023-04-20 16:11:26 +0200

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,81 +16,134 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ignore: non_constant_identifier_names
+  int _PointsA = 0;
+  // ignore: non_constant_identifier_names
+  int _PointsB = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Color.fromARGB(255, 59, 166, 94),
-      //   title: const Text("Birthday Card"),
-      // ),
-      body: Container(
-        color: Colors.teal,
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              CircleAvatar(
-                backgroundImage: AssetImage("assets/imgs/2.JPG"),
-                radius: 100,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Abanoub Emad',
-                style: TextStyle(fontSize: 50, fontFamily: 'Pacifico'),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(fontSize: 30, color: Colors.white70),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 140.0,
-                  vertical: 12,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 79, 56, 172),
+        title: const Text("Basketball Score Counter"),
+        shadowColor: Colors.grey,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 450,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text("Team A", style: TextStyle(fontSize: 28)),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Text('$_PointsA', style: const TextStyle(fontSize: 80)),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsA++;
+                      });
+                    }, "+1 POINT"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsA += 2;
+                      });
+                    }, "+2 POINT"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsA += 3;
+                      });
+                    }, "+3 POINT"),
+                  ],
                 ),
-                child: Divider(
-                  thickness: 2,
-                  height: 1,
+              ),
+              Container(
+                height: 450,
+                color: Colors.grey,
+                width: 2,
+                alignment: Alignment.topCenter,
+              ),
+              SizedBox(
+                height: 450,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text("Team B", style: TextStyle(fontSize: 28)),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Text('$_PointsB', style: const TextStyle(fontSize: 80)),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsB++;
+                      });
+                    }, "+1 POINT"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsB += 2;
+                      });
+                    }, "+2 POINT"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    newElevatedButton(() {
+                      setState(() {
+                        _PointsB += 3;
+                      });
+                    }, "+3 POINT"),
+                  ],
                 ),
               ),
-              SizedBox(height: 10),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 30.0,
-                  vertical: 7,
-                ),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: ListTile(
-                    leading: Icon(Icons.phone),
-                    subtitle: Text("Phone Number"),
-                    title: Text("+20 1152560323"),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 30.0,
-                  vertical: 7,
-                ),
-                child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    child: ListTile(
-                      leading: Icon(Icons.mail),
-                      title: Text("AbanoubEmad@gmail.com"),
-                    )),
-              )
             ],
           ),
-        ),
+          SizedBox(height: 70),
+          Container(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _PointsB = 0;
+                  _PointsA = 0;
+                });
+              },
+              child: Text("reset"),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.grey),
+                  fixedSize: MaterialStateProperty.all(const Size(160, 50))),
+            ),
+          )
+        ],
       ),
+
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
@@ -98,4 +151,35 @@ class _MyHomePageState extends State<MyHomePage> {
       // ),
     );
   }
+
+  ElevatedButton newElevatedButton(fun, text) => ElevatedButton(
+        onPressed: fun,
+        child: Text(text),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+            fixedSize: MaterialStateProperty.all(const Size(210, 50))),
+      );
+
+  // addPoint(team, pCounter) {
+  //   setState(() {
+  //     if (team == 0) {
+  //       if (pCounter == 1) {
+  //         _PointsA++;
+  //       } else if (pCounter == 2) {
+  //         _PointsA += 2;
+  //       } else if (pCounter == 3) {
+  //         _PointsA += 3;
+  //       }
+  //     } else {
+  //       if (pCounter == 1) {
+  //         _PointsB++;
+  //       } else if (pCounter == 2) {
+  //         _PointsB += 2;
+  //       } else if (pCounter == 3) {
+  //         _PointsB += 3;
+  //       }
+  //     }
+  //   });
+  // }
+
 }
